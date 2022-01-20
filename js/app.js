340cd12ff94a14e8adf0db4e7c29b5ae27bc78fd
Anas <an.behhari@gmail.com>
2022-01-20 19:00:19 +0100

    update

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let cart_btn  = document.querySelector(".shopping-bag");
+let cart = document.querySelector(".cart");
+let close_btn = document.querySelector(".close");
+cart_btn.onclick = () => document.body.classList.toggle("show-cart");
+close_btn.onclick = () => document.body.classList.toggle("show-cart");
+
